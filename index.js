9b4183009e5cdc0e86a6ab913d938bdaabf7877e
Dineury <DineuryLopez1@gmail.com>
2025-09-30 13:40:10 -0400

    WIP

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const server = express()
+
+const PORT = process.env.PORT || 9000
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/api/users', (req, res, next) => {
+    res.status(200).json(req.users)
+    next()
+})
+
+server.listen(PORT, () => {
+    console.log('Engines start')
+})
